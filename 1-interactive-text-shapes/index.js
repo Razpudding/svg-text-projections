17c73f5679c7abd4ea887d0be9d43cff188e7a94
Laurens <8708858+Razpudding@users.noreply.github.com>
2020-05-29 16:56:03 +0200

    Draggable groups implemented

--- 1-interactive-text-shapes/index.js
+++ 1-interactive-text-shapes/index.js
@@ -1,35 +1,39 @@
 //SVG Elements
 const input = document.querySelector('input[type=text]')
 const draw = SVG().addTo('#drawing').viewbox(0, 0, 300, 300)
-const nested = draw.nested().dx(100).draggable()
-const group = nested.group()
+const nested = draw.nested().dx(100)
 
 //Settings
 const textSize = 3.5
 const rectangleAmount = 10
 
-//Adding elements
-group.rect(40, 60)
+drawCards(3)
+function drawCards(amount){
+  for (let i = 0; i< amount; i++){
+    const group = nested.nested().draggable().x(i * 50)
+    // group.translate(i * 50, 0)
+    //Adding elements
+    group.rect(40, 60)
 
-//Stuff for setting up a textpath
-const customText = `2Don't like it? Join. Start thinking about the ritual as a game A game in which the rules are not clear take a step, jump. Build a tent. With rope and bed sheets and everything you can find Sc`
-const text = group.text(function(add) {
-  add.tspan(customText)
-})
+    //Stuff for setting up a textpath
+    const customText = `Don't like it? Join. Start thinking about the ritual as a game A game in which the rules are not clear take a step, jump. Build a tent. With rope and bed sheets and everything you can find Sc`
+    const text = group.text(function(add) {
+      add.tspan(customText)
+    })
 
-const word = group.text(function(add) {
-  add.tspan("Photography")
-})
-const customPath ='M 0 10 L 40 10 L 40 60 L 0 60 L 0 0'
-const photographyPos = 'M 0 40 L 40 40'
+    const word = group.text(function(add) {
+      add.tspan("Photography")
+    })
+    const customPath ='M 0 10 L 40 10 L 40 60 L 0 60 L 0 0'
+    const photographyPos = 'M 0 40 L 40 40'
 
-const customTextPath = text.path(customPath).font({ size: textSize, family: 'Verdana' })
-const photographyTextPath = word.path(photographyPos).font({ size: textSize, family: 'Verdana' })
-
-//Code to start later on in the path
-// text.textPath().attr('startOffset', '0%')
-// text.textPath().animate(3000).attr('startOffset', '80%')
-
+    const customTextPath = text.path(customPath).font({ size: textSize, family: 'Verdana' })
+    const photographyTextPath = word.path(photographyPos).font({ size: textSize, family: 'Verdana' })
+  }
+  //Code to start later on in the path
+  // text.textPath().attr('startOffset', '0%')
+  // text.textPath().animate(3000).attr('startOffset', '80%')  
+}
 
 // drawRects(rectangleAmount)
 function drawRects(amount){
@@ -40,9 +44,7 @@ function drawRects(amount){
   rectangles.forEach(rect => rect.animate().rotate(360))
 }
 
-
-input.addEventListener('keyup', updateText(customTextPath))
-
+//input.addEventListener('keyup', updateText(customTextPath))
 function updateText(textPath) {
   return function() {
     textPath.tspan(this.value)
