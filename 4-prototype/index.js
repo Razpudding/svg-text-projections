8a75c4a4b4edcc09cd9ee6801cf5d376f6c35f9b
Laurens <8708858+Razpudding@users.noreply.github.com>
2020-07-17 16:37:44 +0200

    Updated readme and code

--- 4-prototype/index.js
+++ 4-prototype/index.js
@@ -3,6 +3,8 @@ import {settings as S} from './settings.js'
 import {cardContents} from './settings.js'
 
 const container = d3.select("#container")
+const cardsContainer = container.append('g')
+cardsContainer.attr('id', "cardsContainer")
 const containerWidth = parseInt(container.style("width"), 10)
 const containerHeight = parseInt(container.style("height"), 10)
 
@@ -13,30 +15,61 @@ const chairOriginX = originX + (S.circleRadius * Math.sin(0))
 const chairOriginY = originY - (S.circleRadius * Math.cos(0))
 
 const circle = drawCircle()
-drawCards(cardContents, container)
+drawCards(cardContents, cardsContainer)
+drawText(container)
 animate()
 
 //This animates the text along the given path
 function animate(){
   const tl = gsap.timeline();
-  const elements = document.querySelectorAll(".masterTextPath") 
-  console.log(tl)
+  const elements = document.querySelectorAll(".card") 
   
-  elements.forEach( (el, i) => {
-    // console.log(el)
-    let test = 
-    gsap.from(el, S.duration, {
-      attr: { startOffset: "100%"},
-      ease: Power1.easeInOut,
-    })
-    gsap.to('#invitation', S.duration, {
-    	opacity: 1,
- 	 	})
-    tl.add(test)
+  //This will stagger all cards and then show all texts
+  // tl.add('card')
+  // tl.add(gsap.from(".masterTextPath", {
+  //   attr: { startOffset: "100%"},
+  //   ease: Power1.easeInOut,
+  //   stagger: 1
+  // }, 'card')
+  // ).to('.cardTitle', S.duration, {
+  //   opacity: 1,
+  // })
+  
+  //This will stagger all cards and titles separately
+  const textPathAnim = gsap.from('.masterTextPath', S.duration,{
+    attr: { startOffset: "110%"},
+    ease: Power1.easeInOut,
+    stagger: 1
   })
+  const cardTitleAnim = gsap.to('.cardTitle', S.duration, {
+    opacity: 1,
+    stagger: S.duration
+  })
+  //By adding multiple tweens in an array, they are started concurrently
+  tl.add([textPathAnim, cardTitleAnim])
+  
+  //This loops over an array and animates each element individually
+  // elements.forEach( (el, i) => {
+  //   // console.log(el)
+  //   let test = 
+  //   gsap.from(el, S.duration, {
+  //     attr: { startOffset: "100%"},
+  //     ease: Power1.easeInOut,
+  //   })
+  //   gsap.to('.cardTitle', S.duration, {
+  //    opacity: 1,
+  //   })
+  //   tl.add(test)
+  // })
   tl.add(gsap.to('#invitation', S.duration, {
     opacity: 1,
-  }))
+  })
+  ).to("#cardsContainer", 10, {
+    // transformOrigin: `${originX}px ${originY}px`,
+    transformOrigin: '50% 50%',
+    rotation: 360,
+    ease:Linear.easeNone
+  })
   console.log(tl)
 }
 
@@ -44,27 +77,14 @@ function drawCards(contents, container){
   contents.forEach( (card, i) => {
     drawRect(card, container, i)
   })
-  container.append('text')
-  	.text(S.centerText)
-  	.attr('id', 'invitation')
-  	.attr('x', containerWidth /2 - 120)
-  	.attr('y', containerHeight/2)
-  	.attr('fill', 'goldenrod')
-  	.attr('font-size', '24')
 }
 
 //This function adds a path to our svg that we'll later use to animate text along
 // It takes card data, a parent element and a unique id
-function drawRect(card, parent, id){
-  parent = parent.append('g')
+function drawRect(card, target, id){
+  parent = target.append('g')
+  parent.attr('class', 'card')
   console.log(parent)
-  parent
-    .append('text')
-    .attr('fill','#fff')
-    .attr('class', 'cardTitle')
-  	.attr('x', S.cardWidth / 2)
-  	.attr('y', S.cardHeight / 2)
-    .text(card.word)
   
   const rectPath = d3.path()
   rectPath.rect(0,0, S.cardWidth, S.cardHeight)
@@ -72,14 +92,14 @@ function drawRect(card, parent, id){
   //Append the path
   const path = parent.append('path')
   path
-  	.attr('d',rectPath)
+    .attr('d',rectPath)
     .attr('id', 'master' + id)
     .attr('fill','none')
     .attr('stroke','none')
-	
+  
   //Rotate the card in relation to the circles center point
   parent
-  	.attr("transform", 
+    .attr("transform", 
           `rotate( ${(360 /cardContents.length) * id} , ${originX} , ${originY} ) `+
           `translate( ${(chairOriginX - (S.cardWidth / 2))} , ${(chairOriginY - S.cardHeight)} )`)
 
@@ -87,12 +107,20 @@ function drawRect(card, parent, id){
   parent
     .append('text')
     .attr('id', 'mainText')
-  	.attr('fill','#fff')
-  	.append('textPath')
-  		.attr('class', 'masterTextPath')
-  		.attr('href', '#master' + id)
-  		.attr('startOffset', '-100%')
-  		.text(card.text)
+    .attr('fill','#fff')
+    .append('textPath')
+      .attr('class', 'masterTextPath')
+      .attr('href', '#master' + id)
+      .attr('startOffset', '-100%')
+      .text(card.text)
+  
+  parent
+    .append('text')
+    .attr('fill','#fff')
+    .attr('class', 'cardTitle')
+    .attr('x', S.cardWidth / 2)
+    .attr('y', S.cardHeight / 2)
+    .text(card.word) 
 }
 
 function drawCircle(){
@@ -103,4 +131,14 @@ function drawCircle(){
     .attr('r', S.circleRadius)
     .attr('fill', 'none')
     .attr('stroke', "none")
+}
+
+function drawText(target){
+  target.append('text')
+    .text(S.centerText)
+    .attr('id', 'invitation')
+    .attr('x', containerWidth /2)
+    .attr('y', containerHeight/2)
+    .attr('fill', 'goldenrod')
+    .attr('font-size', '24')
 }
--- 4-prototype/readme.md
+++ 4-prototype/readme.md
@@ -3,5 +3,20 @@
 I've created this fork so I can do fancy animations here and let the other viz remain stable.
 
 ## Steps
-
 - Some refactoring: Artist can now edit settings in the settings file.
+
+
+## Choreography
+- There will be a text which contains all of the words in the categories
+- Each card will be composed of words within one category
+- One word withing the category will be displayed in the middle of the card at random
+- The animation duration should be changeable at the top of the code by the artist
+- The whole circle of cards should rotate once, slowly, to highlight each card once on top
+- Some settings can be changed by hand by altering the code. These will be listed as variables at the top of the code for the artist to manipulate
+
+- The number of cards should be changeable. 
+- One word per card at random should be bigger within the textpath and colored differently
+- The text on a card should keep flowing, even when theres no more space left. It can run continuously, this should be changeable.
+- One option should be to have the words in the middle of each card come from one category instead of the card's category. That way a category can be highlighted.
+- The text in the middle should be changeable and will autoscale to fit the available width
+- The final work will be a website, where the artist can copy paste texts, change some settings, and then a visualization will be generated.
